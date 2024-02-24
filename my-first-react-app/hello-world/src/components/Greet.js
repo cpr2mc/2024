8e0b74096d8fe54591b10ffab1ff84c5089e67b5
Chris <roos.chris.p@gmail.com>
2024-02-24 08:35:21 -0600

    added functional and class component to hello world

---
+++ my-first-react-app/hello-world/src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Greet = () => <h1>Hello Chris!</h1>;
+// ^ this syntax is more streamlined, but is imported differently (see App.js)
+
+// export default Greet
+// ^ this syntax is a little more but more explicit or obvious
