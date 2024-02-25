ff21bafd7fbf66165b6531d91339f7d2307e08b9
Chris <roos.chris.p@gmail.com>
2024-02-25 08:31:19 -0600

    quick JS practice session with arrays

---
+++ js-testing/ArrayPractice.js
@@ -0,0 +1,19 @@
+const animals = ['pigs', 'horses', 'sheep', 'armadillos', 'roadrunners'];
+console.log(animals);
+
+// .push() practice
+// const count = animals.push('dogs', 'cats');
+// console.log(count);
+// console.log(animals);
+
+// .pop() practice
+// console.log(animals.pop());
+// console.log(animals)
+
+// .shift() practice
+const count = animals.shift();
+console.log(count)
+
+// .slice() practice
+// console.log(animals.slice(2, 4));
+console.log(animals)
