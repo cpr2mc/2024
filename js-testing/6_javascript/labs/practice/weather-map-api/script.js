c40c875f60184c89a7d14aadbd49edc6b9a4b137
Chris <roos.chris.p@gmail.com>
2024-03-06 14:53:40 -0600

    working on DOM manipulation

--- js-testing/6_javascript/labs/practice/weather-map-api/script.js
+++ js-testing/6_javascript/labs/practice/weather-map-api/script.js
@@ -1,2 +1,20 @@
 // Weather Map API
 
+const x = document.getElementById('#demo');
+    
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude;
+    console.log('showPosition ran.')
+}
+
+// form.addEventListener('submit') {
+//     console.log('button pressed.');
+// }
