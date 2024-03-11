5c1808f1cd958ee5de9afb3b9fe2d6209b09a8d3
Chris <roos.chris.p@gmail.com>
2024-03-10 22:39:42 -0500

    added css, updating functionality

--- js-testing/6_javascript/labs/practice/weather-map-api/script.js
+++ js-testing/6_javascript/labs/practice/weather-map-api/script.js
@@ -1,11 +1,12 @@
 // Weather Map API
 const locationInfo = document.getElementById('location-info');
-const displayButton = document.getElementById('display-location');
+const searchButton = document.getElementById('search-button');
 const weatherInfo = document.getElementById('weather-info')
 const weatherButton = document.getElementById('display-weather');
 let userLatitude = '';
 let userLongitude = '';
     
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -14,37 +15,51 @@ function getLocation() {
     }
 }
 
-function checkResponse(response) {
-    if (!response.ok) {
-        console.error('Weather API call failed:', response.status, response.statusText);
-        weatherInfo.innerHTML = 'Failed to retrieve weather data.';
-        return false;
-    }
-    return true;
-}
-
-async function logWeather(lat, long, apiKey, callback) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
-    const response = await fetch(url);
-    if (!checkResponse(response)) return;
-
-    const weatherData = await response.json();
-    callback(weatherData);
-}
-
 function showPosition(position) {
-    locationInfo.innerHTML = "Latitude: " + position.coords.latitude + "<br> Longitude: " + position.coords.longitude;
+    console.log(position)
     userLatitude = Math.floor(position.coords.latitude * 100) / 100;
     userLongitude = Math.floor(position.coords.longitude * 100) / 100;
+    loadWeather(userLatitude, userLongitude, secretApiKey)
+}
+
+async function loadWeather(lat, long, apiKey) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
+    const response = await fetch(url);
+    // if (!checkResponse(response)) return;
+    
+    var weatherData = await response.json();
+    console.log(weatherData);
+    
+    document.querySelector('.city').innerHTML = weatherData.name;
+    document.querySelector('.temp').innerHTML = Math.floor(weatherData.main.temp) + '°F';
+    document.querySelector('.humidity').innerHTML = weatherData.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = Math.round(weatherData.wind.speed) + ' kts';   
+}
+
+async function searchWeather(cityName, apiKey) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&appid=${apiKey}`
 }
 
 function displayWeather(weatherData) {
     weatherInfo.innerHTML = JSON.stringify(weatherData, null, 2);
 }
 
-displayButton.onclick = getLocation;
+searchButton.onclick = searchWeather;
 
 weatherButton.onclick = function () {
-    logWeather(userLatitude, userLongitude, secretApiKey, displayWeather)
+    displayWeather(weatherData)
 }
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    getLocation();
+});  
+
+// function checkResponse(response) {
+    //     if (!response.ok) {
+        //         console.error('Weather API call failed:', response.status, response.statusText);
+        //         weatherInfo.innerHTML = 'Failed to retrieve weather data.';
+        //         return false;
+        //     }
+        //     return true;
+        // }
+        
