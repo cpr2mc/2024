698fa31b328807159eef978cccfd49097c5787df
Chris <roos.chris.p@gmail.com>
2024-03-11 13:37:36 -0500

    finished weather app

--- js-testing/6_javascript/labs/practice/weather-map-api/script.js
+++ js-testing/6_javascript/labs/practice/weather-map-api/script.js
@@ -1,65 +1,87 @@
 // Weather Map API
 const locationInfo = document.getElementById('location-info');
-const searchButton = document.getElementById('search-button');
-const weatherInfo = document.getElementById('weather-info')
-const weatherButton = document.getElementById('display-weather');
-let userLatitude = '';
-let userLongitude = '';
-    
+const weatherInfo = document.getElementById('weather-info');
+const citySearch = document.querySelector('.search input');
+const searchButton = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon')
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        locationInfo.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    console.log(position)
-    userLatitude = Math.floor(position.coords.latitude * 100) / 100;
-    userLongitude = Math.floor(position.coords.longitude * 100) / 100;
-    loadWeather(userLatitude, userLongitude, secretApiKey)
-}
-
-async function loadWeather(lat, long, apiKey) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
-    const response = await fetch(url);
-    // if (!checkResponse(response)) return;
-    
+async function printWeather(response) {
     var weatherData = await response.json();
     console.log(weatherData);
     
     document.querySelector('.city').innerHTML = weatherData.name;
-    document.querySelector('.temp').innerHTML = Math.floor(weatherData.main.temp) + '°F';
+    document.querySelector('.temp').innerHTML = Math.round(weatherData.main.temp) + '°F';
     document.querySelector('.humidity').innerHTML = weatherData.main.humidity + '%';
-    document.querySelector('.wind').innerHTML = Math.round(weatherData.wind.speed) + ' kts';   
+    document.querySelector('.wind').innerHTML = Math.round(weatherData.wind.speed) + ' kts';
+
+    weatherIcon.src = 'images/' + weatherData.weather[0].main + '.png'
+
+    document.querySelector('.weather').style.display = 'block'
 }
 
-async function searchWeather(cityName, apiKey) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${cityName}&appid=${apiKey}`
+async function searchWeather(city, apiKey) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(url);
+    
+    if(response.status == 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+    } else {
+        printWeather(response);
+    }
 }
 
-function displayWeather(weatherData) {
-    weatherInfo.innerHTML = JSON.stringify(weatherData, null, 2);
-}
+searchButton.onclick = () => {
+    document.querySelector('.error').style.display = 'none';
+    searchWeather(citySearch.value, secretApiKey);
+};
 
-searchButton.onclick = searchWeather;
+citySearch.addEventListener('keypress', function(event) {
+    if (event.key === "Enter") {
+        document.querySelector('.error').style.display = 'none';
+        searchWeather(citySearch.value, secretApiKey);
+    }
+});
 
-weatherButton.onclick = function () {
-    displayWeather(weatherData)
-}
+/** ------------------------------------------------------------------------------------------------------------------- */
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    getLocation();
-});  
+/** Version 1: Gets user's location (if allowed) and automatically displays it */
+
+// let userLatitude = '';
+// let userLongitude = '';
+
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         locationInfo.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+                    
+// function showPosition(position) {
+//     console.log(position)
+//     userLatitude = Math.floor(position.coords.latitude * 100) / 100;
+//     userLongitude = Math.floor(position.coords.longitude * 100) / 100;
+//     loadWeather(userLatitude, userLongitude, secretApiKey)
+// }
+// async function loadWeather(lat, long, apiKey) {
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
+//     const response = await fetch(url);
+//     // if (!checkResponse(response)) return;
+    
+//     printWeather(response);
+// }
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     getLocation();
+// });  
 
 // function checkResponse(response) {
-    //     if (!response.ok) {
-        //         console.error('Weather API call failed:', response.status, response.statusText);
-        //         weatherInfo.innerHTML = 'Failed to retrieve weather data.';
-        //         return false;
-        //     }
-        //     return true;
-        // }
+//     if (!response.ok) {
+    //         console.error('Weather API call failed:', response.status, response.statusText);
+    //         weatherInfo.innerHTML = 'Failed to retrieve weather data.';
+    //         return false;
+    //     }
+    //     return true;
+    // }
         
