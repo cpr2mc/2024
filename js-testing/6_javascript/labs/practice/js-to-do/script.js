66a8df90437faa6561d768bcab49406f83909912
Chris <roos.chris.p@gmail.com>
2024-03-12 21:29:35 -0500

    finished to do app -- js localstorage demo

--- js-testing/6_javascript/labs/practice/js-to-do/script.js
+++ js-testing/6_javascript/labs/practice/js-to-do/script.js
@@ -1,2 +1,41 @@
 // To Do List App - https://www.youtube.com/watch?v=G0jO8kUrg-I&t=1s
+// demonstrates localStorage functionality
 
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+// I do not like the below way of doing this.. might need a rewrite. 
+// Never seem the DOMTokenList properties used that way.
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if(e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+};
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask();
