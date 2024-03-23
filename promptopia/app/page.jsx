a6ce0bcab406893ab1ef4d06248c1e9126813a10
Chris <roos.chris.p@gmail.com>
2024-03-23 15:06:48 -0500

    started the full stack development video

---
+++ promptopia/app/page.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & Share
+        </h1>
+    </section>
+  )
+}
+
+export default Home
