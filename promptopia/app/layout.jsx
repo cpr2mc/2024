5357435a8409dd3c8c2d8f3cda40dc4feebed4b7
Chris <roos.chris.p@gmail.com>
2024-03-24 23:07:28 -0500

    continued next vid

--- promptopia/app/layout.jsx
+++ promptopia/app/layout.jsx
@@ -1,6 +1,9 @@
 import '@styles/globals.css'
 import { children } from 'react'
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
 export const metadata = {
     title: "Promptopia",
     description: 'Discover & Share AI Prompts'
@@ -14,6 +17,7 @@ const RootLayout = ({ children }) => {
                 <div className="gradient" />
             </div>
             <main className="app">
+                <Nav />
                 {children}
             </main>
         </body>
