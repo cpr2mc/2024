7fb5d06768c4a38524bf9ffb3b8dce257dffe765
Chris <roos.chris.p@gmail.com>
2024-03-26 09:41:13 -0500

    worked on Nav functionality and mobile Nav

--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -1,4 +1,4 @@
-import Feed from "@components/Feed"
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
@@ -6,7 +6,7 @@ const Home = () => {
         <h1 className="head_text text-center">
             Discover & Share
             <br className="max-md:hidden"/>
-            <span className="orange_gradient text-center"> AI-powered Prompts</span>
+            <span className="orange_gradient text-center"> AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
             Promptopia is an open-source AI prompting tool for the modern world to discover, create, and share creative prompts
